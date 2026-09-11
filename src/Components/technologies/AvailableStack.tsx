b8013a31f75f23dcd4ab6f809504174c3e28c53b
pseudo77 <suedo7721@gmial.com>
2026-09-12 05:42:28 +0600

    AvailableStack Modified

--- src/Components/technologies/AvailableStack.tsx
+++ src/Components/technologies/AvailableStack.tsx
@@ -1,1 +1,33 @@
 
+import type { Itechnology } from "../type/Type";
+import TechnologyCard from "./TechnologyCard";
+
+
+
+
+interface IavailableProps{
+    technologies:Itechnology[]
+    handleSelectedTechno:()=>void;
+}
+
+const AvailableStack = ({technologies,handleSelectedTechno}:IavailableProps) => {
+    return (
+        <div>
+            <div className="grid grid-cols-3 gap-10 roun ">
+                {
+                    technologies.map((technology:Itechnology)=>{
+                        return (
+                            <div key={technology.id} >
+                                <TechnologyCard technology={technology} handleSelectedTechno={handleSelectedTechno}></TechnologyCard>
+                                
+                   
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+};
+
+export default AvailableStack;
